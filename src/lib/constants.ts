c3862b72ade879038127b0f628464dfba71117a6
Savy <savy.novachez011@proton.me>
2025-09-03 15:17:30 +0530

    add challenge constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const CHALLENGE_URL = 'https://tns4lpgmziiypnxxzel5ss5nyu0nftol.lambda-url.us-east-1.on.aws/challenge';
+
+export const SectionRegex = new RegExp(/^92\S+/);
+export const ArticleRegex = new RegExp(/\S+45$/);
+export const DivRegex = new RegExp(/\S+78\S+/);
