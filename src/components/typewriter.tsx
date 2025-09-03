a085dbae685d21966969914fdc52a39ecc2af7d8
Savy <savy.novachez011@proton.me>
2025-09-03 16:27:52 +0530

    create typewriter component

---
+++ src/components/typewriter.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { useTimeout } from '../lib/hooks';
+
+interface Props {
+  flag: string;
+}
+
+interface CharProps {
+  delay: number;
+  character: string;
+}
+
+function Char({ delay, character }: CharProps) {
+  const [show, setShow] = useState(false);
+
+  useTimeout(() => {
+    setShow(!show);
+  }, delay)
+
+  return (
+    <span style={{ display: show ? 'inline' : 'none' }}>{character}</span>
+  )
+}
+
+export default function Typewriter({ flag }:Props) {
+  const flagList = flag.split('');
+  
+  return (
+    <p style={{ fontFamily: 'monospace', }}>
+      {flagList.map((char, idx) => (
+        <Char key={`${char}-${idx}`} character={char} delay={idx * 500}/>
+      ))}
+    </p>
+  )
+}
